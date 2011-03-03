703f5706f11966366e6afa094feb263e270fc22d
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-03-03 20:57:16 +0000

    Initial commit of the generated android app files.

---
+++ calc/src/net/marcuswhybrow/uni/g54mdp/ae01/calc/Calc.java
@@ -0,0 +1,15 @@
+package net.marcuswhybrow.uni.g54mdp.ae01.calc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Calc extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
