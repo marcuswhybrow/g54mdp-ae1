fe3d22e12bc99afd077c067e8456dffbf2c95d46
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-03-08 12:23:37 +0000

    Basic calculator functions now all work, buttons stretch to fill screen.

--- calc/src/net/marcuswhybrow/uni/g54mdp/ae01/calc/Calc.java
+++ calc/src/net/marcuswhybrow/uni/g54mdp/ae01/calc/Calc.java
@@ -9,6 +9,29 @@ import android.widget.AdapterView.OnItemClickListener;
 
 public class Calc extends Activity
 {
+    private enum State { INITIAL, NUMBER, OPERATION, ANSWER }
+    private enum Operation { DIVIDE, MULTIPLY, SUBTRACT, ADD }
+    
+    private State state = State.INITIAL;
+    private Operation operation = null;
+    
+    private float previous;
+    private int previousLength;
+    
+    private Button digitButtons[];
+    private Button operationButtons[];
+    
+    private Button operationEquals;
+    private Button operationDivide;
+    private Button operationMultiply;
+    private Button operationSubtract;
+    private Button operationAdd;
+    
+    private void setPrevious(CharSequence s) {
+        this.previous = Float.valueOf(s.toString()).floatValue();
+        this.previousLength = s.length();
+    }
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -18,7 +41,7 @@ public class Calc extends Activity
         
         final TextView display = (TextView) findViewById(R.id.display);
         
-        Button digitButtons[] = {
+        digitButtons = new Button[] {
             (Button) findViewById(R.id.digit0),
             (Button) findViewById(R.id.digit1),
             (Button) findViewById(R.id.digit2),
@@ -32,13 +55,13 @@ public class Calc extends Activity
             (Button) findViewById(R.id.digitPeriod)
         };
         
-        Button operationEquals = (Button) findViewById(R.id.operationEquals);
-        Button operationDivide = (Button) findViewById(R.id.operationDivide);
-        Button operationMultiply = (Button) findViewById(R.id.operationMultiply);
-        Button operationSubtract = (Button) findViewById(R.id.operationSubtract);
-        Button operationAdd = (Button) findViewById(R.id.operationAdd);
+        operationEquals = (Button) findViewById(R.id.operationEquals);
+        operationDivide = (Button) findViewById(R.id.operationDivide);
+        operationMultiply = (Button) findViewById(R.id.operationMultiply);
+        operationSubtract = (Button) findViewById(R.id.operationSubtract);
+        operationAdd = (Button) findViewById(R.id.operationAdd);
         
-        Button operationButtons[] = {
+        operationButtons = new Button[] {
             operationDivide,
             operationMultiply,
             operationSubtract,
@@ -49,10 +72,17 @@ public class Calc extends Activity
             button.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     Button b = (Button) v;
-                    if ("0".equals(display.getText()))
-                        display.setText(b.getText());
-                    else
-                        display.append(b.getText());
+                    switch(state) {
+                        case INITIAL:
+                        case ANSWER:
+                            display.setText(b.getText());
+                            break;
+                        case OPERATION:
+                        case NUMBER:
+                            display.append(b.getText());
+                            break;
+                    }
+                    state = State.NUMBER;
                 }
             });
         }
@@ -61,18 +91,61 @@ public class Calc extends Activity
             button.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     Button b = (Button) v;
-                    display.setText(b.getText());
+                    setPrevious(display.getText());
+                    CharSequence operator = b.getText();
+                    display.append(operator);
+                    
+                    switch(operator.toString().charAt(0)) {
+                        case '÷':
+                            operation = Operation.DIVIDE;
+                            break;
+                        case '×':
+                            operation = Operation.MULTIPLY;
+                            break;
+                        case '−':
+                            operation = Operation.SUBTRACT;
+                            break;
+                        case '+':
+                            operation = Operation.ADD;
+                            break;
+                    }
+                    
+                    state = State.OPERATION;
                 }
             });
         }
         
         operationEquals.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Button b = (Button) v;
-                display.setText(b.getText());
+                if (state == State.NUMBER) {
+                    Button b = (Button) v;
+                    float current = Float.valueOf(display.getText().toString().substring(previousLength+1)).floatValue();
+                    float result = 0.0f;
+                    switch(operation) {
+                        case DIVIDE:
+                            result = previous / current;
+                            break;
+                        case MULTIPLY:
+                            result = previous * current;
+                            break;
+                        case SUBTRACT:
+                            result = previous - current;
+                            break;
+                        case ADD:
+                            result = previous + current;
+                            break;
+                    }
+                    
+                    String visualResult;
+                    if ((int) result == result)
+                        visualResult = Integer.toString((int) result);
+                    else
+                        visualResult = Float.toString(result);
+                    
+                    display.setText(visualResult);
+                    state = State.ANSWER;
+                }
             }
         });
-        
-        
     }
 }
